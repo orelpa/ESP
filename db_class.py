c6f8debc8a36a74318d1b1ac67c7f8b49a6bef56
orelpa <pa.orel@yandex.ru>
2024-03-05 09:38:50 +0300

    Создан класс DB Добавлены методы подключение проверка даты создание новой записи, для нового дня просмотр занчений метод обновления начений

---
+++ db_class.py
@@ -0,0 +1,111 @@
+import psycopg2
+
+msg = "Тестовый принт"
+from psycopg2 import Error
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+class DB:
+    'Модель подключения, чтения, записи в БД'
+    def __init__(self, user, password, host, port):
+        self.user = user
+        self.password = password
+        self.host = host
+        self.port = port
+
+        #Соединяемся с БД
+        self.con = psycopg2.connect(user=self.user, password=self.password, host=self.host, port=self.port)
+
+        # Автоматическое сохранение изменений
+        self.con.autocommit = True
+        #создаём курсор для работы с БД
+        self.cur = self.con.cursor()
+
+        #создаём таблицу если её не существует
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS count (id serial PRIMARY KEY, box INTEGER, paper INTEGER, posting_date DATE NOT NULL DEFAULT CURRENT_DATE)"
+        )
+        #self.cur.execute(
+         #   "INSERT INTO count(box, paper) VALUES ( '0', '0' );"
+        #)
+
+
+    def insert(self,id, box, paper):
+
+        self.cur.execute(
+            "INSERT INTO count(id, box, paper) VALUES ('%s', '%s', '%s' );"%(id,box,paper)
+        )
+
+    def new_day_insert(self):
+        "Метод создания новой сторки на новый день"
+        self.cur.execute(
+                "INSERT INTO count(box, paper) VALUES ('0', '0');"
+        )
+
+
+
+    def view(self):
+        'Просмотр всех значений в БД'
+        self.cur.execute(
+            "SELECT * FROM count"
+        )
+        rows = self.cur.fetchall()
+        return rows
+
+    def check_valid_date(self):
+        #получение последней записи даты
+        self.cur.execute(
+            "SELECT EXISTS(SELECT posting_date FROM count)" #Запрашивает существоание записи возвращает True False
+        )
+        last_posted_data_value = self.cur.fetchone()[0]
+        if last_posted_data_value:
+            self.cur.execute(
+                "SELECT posting_date FROM count"
+            )
+            last_posted_data =self.cur.fetchone()[0]
+        else:
+            print(msg + "Текущей даты нет, создаём запись")
+            self.new_day_insert()
+            self.cur.execute(
+                "SELECT posting_date FROM count"
+            )
+            last_posted_data = self.cur.fetchone()[0]
+
+        #получение текущей даты
+
+
+
+        self.cur.execute(
+            "SELECT NOW()::DATE;"
+        )
+        current_posted_date = self.cur.fetchone()[0]
+
+        if current_posted_date == last_posted_data:
+            print("Тестовый вывод: Сегодняшняя дата есть")
+
+        else:
+            self.new_day_insert()
+            #"INSERT INTO count(id, box, paper) VALUES ('%s', '%s', '%s' );"%(0,0,0)
+            print(msg + " Текущей даты нет, делаем новую запись")
+
+    def update(self, used):
+        "Метод обновлят записи box и paper в БД"
+        match used:
+            case "box":
+                self.cur.execute(
+                    "UPDATE count SET box = box +1"
+                )
+            case "paper":
+                self.cur.execute(
+                    "UPDATE count SET paper = paper +1"
+                )
+
+
+
+
+
+my_bd = DB("postgres", "postgres", "127.0.0.1", "5432")
+
+
+my_bd.check_valid_date()
+my_bd.update("paper")
+
